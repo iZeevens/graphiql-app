de9c1d9f0b30d6821fb0c98ce60ac2202531c87c
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-08-22 03:57:53 +0500

    feat: add react hook form for signUpForm

--- graphiql-app/src/components/SignUpForm/SignUpForm.tsx
+++ graphiql-app/src/components/SignUpForm/SignUpForm.tsx
@@ -1,7 +1,23 @@
+'use client';
 import { Box, Button, TextField, Typography } from '@mui/material';
 import { SERVICE_MESSAGES } from '../../constants/SERVICE_MESSAGES';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface Inputs {
+  email: string;
+  password: string;
+}
 
 const SignUpForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = data => {
+    console.log(data);
+  };
+
   return (
     <Box
       component='div'
@@ -24,11 +40,25 @@ const SignUpForm = () => {
           flexDirection: 'column',
           rowGap: '25px',
         }}
+        onSubmit={handleSubmit(onSubmit)}
       >
-        <TextField id='form-email' label='Email' />
-        <TextField type='password' id='form-password' label='Password' />
-        <Button size='large' variant='contained'>
-          {SERVICE_MESSAGES.signInText}
+        <TextField
+          id='form-email'
+          label='Email'
+          {...register('email')}
+          error={!!errors.email}
+          helperText={errors.email?.message}
+        />
+        <TextField
+          type='password'
+          id='form-password'
+          label='Password'
+          {...register('password')}
+          error={!!errors.password}
+          helperText={errors.email?.message}
+        />
+        <Button type='submit' size='large' variant='contained'>
+          {SERVICE_MESSAGES.signUpText}
         </Button>
       </Box>
     </Box>
