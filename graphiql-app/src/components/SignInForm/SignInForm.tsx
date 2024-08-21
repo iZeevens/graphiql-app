970d365c1cfe592334f2272e8e922cae599c5dde
maybe-skiFF <den.skiff190@gmail.com>
2024-08-21 22:32:30 +0300

    feat: add basic sign-in form markup

--- graphiql-app/src/components/SignInForm/SignInForm.tsx
+++ graphiql-app/src/components/SignInForm/SignInForm.tsx
@@ -1,7 +1,38 @@
-import { Box } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
+import { SERVICE_MESSAGES } from '../../constants/SERVICE_MESSAGES';
 
 const SignInForm = () => {
-  return <Box></Box>;
+  return (
+    <Box
+      component='div'
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        m: ' 0 auto',
+      }}
+    >
+      <Typography component='h1' variant='h4'>
+        {SERVICE_MESSAGES.signInText}
+      </Typography>
+      <Box
+        component='form'
+        sx={{
+          my: '20px',
+          display: 'flex',
+          flexDirection: 'column',
+          rowGap: '25px',
+        }}
+      >
+        <TextField id='form-email' label='Email' />
+        <TextField type='password' id='form-password' label='Password' />
+        <Button size='large' variant='contained'>
+          {SERVICE_MESSAGES.signInText}
+        </Button>
+      </Box>
+    </Box>
+  );
 };
 
 export { SignInForm };
---
+++ graphiql-app/src/constants/SERVICE_MESSAGES.ts
@@ -0,0 +1,5 @@
+const SERVICE_MESSAGES = {
+  signInText: 'Sign In',
+};
+
+export { SERVICE_MESSAGES };
