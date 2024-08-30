5b0aab2a3f0bb75fd10e38ed0ae49cd490f5a363
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-08-31 01:44:49 +0500

    feat: add change to default text

--- graphiql-app/src/components/CodeMirror/CodeMirror.tsx
+++ graphiql-app/src/components/CodeMirror/CodeMirror.tsx
@@ -1,8 +1,19 @@
 'use client';
 
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from '@mui/material';
+
+import { useState } from 'react';
+
 import { json, jsonParseLinter } from '@codemirror/lang-json';
 import { linter } from '@codemirror/lint';
 import CodeMirror from '@uiw/react-codemirror';
+import { basicSetup } from 'codemirror';
 
 interface ICodePreviewProps {
   body: string;
@@ -10,12 +21,37 @@ interface ICodePreviewProps {
 }
 
 const CodePreview = ({ body, onChange }: ICodePreviewProps) => {
+  const [lang, setLang] = useState('text');
+
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    setLang(event.target.value);
+    console.log(event.target.value);
+  };
+
   return (
-    <CodeMirror
-      value={body}
-      onChange={onChange}
-      extensions={[json(), linter(jsonParseLinter())]}
-    />
+    <>
+      <FormControl fullWidth sx={{ width: '20%', marginBottom: '10px' }}>
+        <InputLabel id='lang-label'>Language</InputLabel>
+        <Select
+          labelId='lang-label'
+          label='Language'
+          fullWidth
+          variant='outlined'
+          defaultValue='text'
+          onChange={handleChange}
+        >
+          <MenuItem value='text'>Text</MenuItem>
+          <MenuItem value='json'>Json</MenuItem>
+        </Select>
+      </FormControl>
+      <CodeMirror
+        value={body}
+        onChange={onChange}
+        extensions={
+          lang === 'json' ? [json(), linter(jsonParseLinter())] : [basicSetup]
+        }
+      />
+    </>
   );
 };
 
