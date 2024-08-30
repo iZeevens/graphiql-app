c03e349bc6d3667e28330eda4cbc9f0f618decb4
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-08-30 20:06:47 +0500

    feat: add some styles, change layout

--- graphiql-app/src/components/RESTfull/RESTfull.tsx
+++ graphiql-app/src/components/RESTfull/RESTfull.tsx
@@ -3,20 +3,22 @@ import {
   Button,
   Card,
   CardContent,
+  FormControl,
   Grid,
+  InputLabel,
   MenuItem,
   Select,
   TextField,
   Typography,
 } from '@mui/material';
 
-// import JSONPretty from 'react-json-pretty';
+import styles from '@/components/RESTfull/RESTfull.module.scss';
 
 // Базовая разметка, она скоро поменяется
 
 const Restfull = () => {
   return (
-    <Box sx={{ p: 3 }}>
+    <Box sx={{ p: 3 }} className={styles['restfull-client']}>
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <Card component='form'>
@@ -25,31 +27,51 @@ const Restfull = () => {
                 REST Client
               </Typography>
 
-              <Grid container spacing={2}>
-                <Grid item xs={4}>
-                  <Select label='Method' fullWidth variant='outlined'>
-                    {[
-                      'GET',
-                      'PUT',
-                      'POST',
-                      'DELETE',
-                      'PATCH',
-                      'HEAD',
-                      'OPTIONS',
-                      'TRACE',
-                    ].map(option => (
-                      <MenuItem key={option} value={option}>
-                        {option}
-                      </MenuItem>
-                    ))}
-                  </Select>
+              <Grid
+                container
+                className={styles['restfull-client__url-container']}
+              >
+                <Grid item xs={2}>
+                  <FormControl fullWidth variant='outlined'>
+                    <InputLabel id='method-label'>Method</InputLabel>
+                    <Select
+                      labelId='method-label'
+                      label='Method'
+                      fullWidth
+                      variant='outlined'
+                    >
+                      {[
+                        'GET',
+                        'PUT',
+                        'POST',
+                        'DELETE',
+                        'PATCH',
+                        'HEAD',
+                        'OPTIONS',
+                        'TRACE',
+                      ].map(option => (
+                        <MenuItem key={option} value={option}>
+                          {option}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
                 </Grid>
-                <Grid item xs={8}>
+                <Grid className={styles['restfull-client__url']} item xs>
                   <TextField
                     label='Endpoint URL'
                     fullWidth
                     variant='outlined'
                   />
+                </Grid>
+                <Grid item>
+                  <Button
+                    className={styles['restfull-client__button']}
+                    variant='contained'
+                    color='primary'
+                  >
+                    Send Request
+                  </Button>
                 </Grid>
               </Grid>
 
@@ -79,12 +101,6 @@ const Restfull = () => {
                   rows={4}
                 />
               </Box>
-
-              <Box mt={2}>
-                <Button variant='contained' color='primary'>
-                  Send Request
-                </Button>
-              </Box>
             </CardContent>
           </Card>
         </Grid>
@@ -105,8 +121,6 @@ const Restfull = () => {
                   readOnly: true,
                 }}
               />
-
-              {/* <JSONPretty /> */}
             </CardContent>
           </Card>
         </Grid>
