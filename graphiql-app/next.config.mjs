094319ae1d09099d8228126fcf59aec149058434
maybe-skiFF <den.skiff190@gmail.com>
2024-09-03 23:12:10 +0300

    feat: setup jest for a project

--- graphiql-app/next.config.mjs
+++ graphiql-app/next.config.mjs
@@ -1,6 +1,8 @@
 import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+};
 
 export default withNextIntl(nextConfig);
