530d10dd803a6a24d6d4d4b3e79ede3b321c0523
Anastasiia Koval <anastasiiakovalklkl@gmail.com>
2024-09-05 00:46:21 +0300

    feat: implement project info

---
+++ graphiql-app/src/components/welcome/Welcome.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import Container from '@mui/material/Container';
+
+import Greeting from '@/components/welcome/components/greeting/Greeting';
+import { ProjectInfo } from '@/components/welcome/components/projectInfo/ProjectInfo';
+import { useAuth } from '@/hooks/useAuth';
+
+export function Welcome() {
+  const { user, loading } = useAuth();
+
+  return (
+    <>
+      {loading ? (
+        <Container sx={{ textAlign: 'center' }}>Loading...</Container>
+      ) : (
+        <Greeting user={user} />
+      )}
+      <ProjectInfo />
+    </>
+  );
+}
---
+++ graphiql-app/src/components/welcome/components/projectInfo/ProjectInfo.tsx
@@ -0,0 +1,31 @@
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+import { useTranslations } from 'next-intl';
+
+import styles from '@/components/welcome/components/projectInfo/ProjectInfo.module.scss';
+
+export function ProjectInfo() {
+  const t = useTranslations('projectInfo');
+  const infoLines = t('info') ? t('info').split('\n') : [];
+
+  return (
+    <Box className={styles.projectInfo} component='section'>
+      <Container>
+        <Typography className={styles.projectInfo__title} variant='h2'>
+          {t('title')}
+        </Typography>
+        {infoLines.map((line, index) => (
+          <Typography
+            className={styles.projectInfo__text}
+            key={index}
+            paragraph
+          >
+            {line}
+          </Typography>
+        ))}
+      </Container>
+    </Box>
+  );
+}
