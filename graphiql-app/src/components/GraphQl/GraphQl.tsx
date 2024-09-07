96ae2fa930e05d209a1cc111f875ec84d0881da0
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-09-07 19:32:29 +0500

    feat: add urls, btn send request

--- graphiql-app/src/components/GraphQl/GraphQl.tsx
+++ graphiql-app/src/components/GraphQl/GraphQl.tsx
@@ -1,14 +1,32 @@
-import { Box, Card, Grid, Typography } from '@mui/material';
+import { Box, Button, Card, Grid, TextField, Typography } from '@mui/material';
+
+// import styles from '@/components/GraphQl/GraphQl.module.scss';
 
 const GraphQl = () => {
   return (
     <Box sx={{ p: 3 }}>
-      <Grid container>
-        <Grid item>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
           <Card component='form'>
             <Typography variant='h6' gutterBottom>
               GraphQl Client
             </Typography>
+
+            <Grid container>
+              <Grid item>
+                <TextField label='Endpoint URL' fullWidth variant='outlined' />
+              </Grid>
+              <Grid item>
+                <TextField label='SDL URL' fullWidth variant='outlined' />
+              </Grid>
+              {/* Headers RESTFULL & CodeMirror & variables  will be here */}
+
+              <Grid item>
+                <Button variant='contained' color='primary' type='submit'>
+                  Send Request
+                </Button>
+              </Grid>
+            </Grid>
           </Card>
         </Grid>
       </Grid>
