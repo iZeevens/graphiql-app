9bdf3ae360c7e21ce6e2ace6b4c626cd389a90e0
iZeevens <129598659+iZeevens@users.noreply.github.com>
2024-09-09 19:08:40 +0500

    fix: layout

--- graphiql-app/src/app/layout.tsx
+++ graphiql-app/src/app/layout.tsx
@@ -1,11 +1,42 @@
-export default function RootLayout({
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+
+import '@/app/globals.scss';
+import Footer from '@/components/footer/footer';
+import Header from '@/components/header/header';
+import { theme } from '@/theme';
+import type { Metadata } from 'next';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from 'next-intl/server';
+
+export const metadata: Metadata = {
+  title: 'GraphiQL App',
+};
+
+export default async function RootLayout({
   children,
-}: {
+  params: { locale },
+}: Readonly<{
   children: React.ReactNode;
-}) {
+  params: { locale: string };
+}>) {
+  const messages = await getMessages();
+
   return (
-    <html>
-      <body>{children}</body>
+    <html lang={locale}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <body className='flex flex-col min-h-screen'>
+          <AppRouterCacheProvider>
+            <NextIntlClientProvider messages={messages}>
+              <Header />
+              <main className='flex-grow'>{children}</main>
+              <Footer />
+            </NextIntlClientProvider>
+          </AppRouterCacheProvider>
+        </body>
+      </ThemeProvider>
     </html>
   );
 }
