de4687737c739c6ed7a3f3fe0c8ceeaa650f67a0
Anastasiia Koval <anastasiiakovalklkl@gmail.com>
2024-09-13 00:45:25 +0300

    feat: add store utils

---
+++ graphiql-app/src/utils/localStorage.ts
@@ -0,0 +1,13 @@
+export const store = {
+  hasItem: (key: string): boolean => {
+    return Boolean(localStorage.getItem(key));
+  },
+
+  getItem: (key: string): string | null => {
+    return localStorage.getItem(key);
+  },
+
+  setItem: (key: string, value: string): void => {
+    localStorage.setItem(key, value);
+  },
+};
